d930b22a528d59af40fe4c3155c5c56b55971d02
Nisa Delgado <nisadelgado@gmail.com>
2021-11-02 00:07:30 -0400

    PHPUnit integrado

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+	public function test_example()
+	{
+		return $this->assertEquals(1, 1);
+	}
+}
