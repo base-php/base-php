55285323d5d2dd0a16a7bf6c07cc9c3718e2b972
Nisa Delgado <nisadelgado@gmail.com>
2022-02-17 08:45:53 -0400

    Autoenfoque en formularios

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,1 +1,1 @@
-<input name="{{ $name ?? '' }}" {{ $required ?? '' }} type="{{ $type ?? 'text' }}" value="{{ $value ?? '' }}" class="{{ $class ?? '' }} block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+<input autofocus name="{{ $name ?? '' }}" {{ $required ?? '' }} type="{{ $type ?? 'text' }}" value="{{ $value ?? '' }}" class="{{ $class ?? '' }} block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
