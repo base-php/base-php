10319a97e128b1b5ff847b98c875d6f6b9fe747e
Nisa Delgado <nisadelgado@gmail.com>
2022-03-15 08:39:38 -0400

    Cambio de estilo a inputs

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
 
         <div class="grid grid-cols-2 mb-5">
             <div>
-                <input x-on:keyup="search($el)" autofocus type="text" placeholder="{{ lang('users.search') }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <input x-on:keyup="search($el)" autofocus type="text" placeholder="{{ lang('users.search') }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
 
             <div class="text-right">
